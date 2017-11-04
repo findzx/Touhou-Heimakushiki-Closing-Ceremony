e7d1cb54ae05d47e8a617ad2d66579911f4a3f3f
findzx <861145069@qq.com>
2017-11-04 17:11:26 -0400

    删掉了一些没用的图，改了基本UI表层

--- lang/zh_CN/BasaraMode.lua
+++ lang/zh_CN/BasaraMode.lua
@@ -1,13 +1,13 @@
--- translations for Basara mode
+﻿-- translations for Basara mode
 
 return
 {
 	["Basara"] = "暗将",
-	["#BasaraReveal"] = "%from 展示了武将，主将为 %arg，副将为 %arg2",
-	["RevealGeneral"] = "展示武将",
+	["#BasaraReveal"] = "%from 展示了角色，主将为 %arg，副将为 %arg2",
+	["RevealGeneral"] = "展示角色",
 	["anjiang"] = "暗将",
-	["#BasaraGeneralChosen"] = "你选择的武将为 %arg",
-	["#BasaraGeneralChosenDual"] = "你选择的武将为 %arg 和 %arg2",
+	["#BasaraGeneralChosen"] = "你选择的角色为 %arg",
+	["#BasaraGeneralChosenDual"] = "你选择的角色为 %arg 和 %arg2",
 	["Hegemony"] = "国战",
 	["Roles"] = "身份",
 	["~anjiang"] = "死不瞑目啊……",
